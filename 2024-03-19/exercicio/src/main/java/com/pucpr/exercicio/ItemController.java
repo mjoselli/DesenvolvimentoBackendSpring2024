28e67157c537596966b20fef2d80d3c26c84c169
mjoselli <mejoselli@gmail.com>
2024-03-19 20:44:45 -0300

    Aula 19/03/2024

---
+++ 2024-03-19/exercicio/src/main/java/com/pucpr/exercicio/ItemController.java
@@ -0,0 +1,39 @@
+package com.pucpr.exercicio;
+
+import org.springframework.web.bind.annotation.RestController;
+/*
+Objetivo: Implementar um CRUD completo para um Array Map de itens no Spring Boot, utilizando Java.
+Funcionalidades:
+* Listar todos os itens: Exibir todos os itens presentes no Array Map.
+* Buscar um item por ID: Obter um item específico pelo seu índice no Array Map.
+* Adicionar um novo item: Inserir um novo item no final do Array Map.
+* Atualizar um item: Substituir um item existente no Array Map pelo índice.
+* Remover um item: Excluir um item do Array Map pelo seu índice.
+Instruções:
+1. Crie um projeto Spring Boot com as dependências Web.
+2. Defina uma classe Item com os atributos nome, quantidade, valor e descritivo.
+3. Implemente um Controller RESTful ItemController com os seguintes endpoints:
+    * GET /api/itens: Retorna todos os itens do Array Map.
+    * GET /api/itens/{id}: Retorna o item com o índice especificado (id).
+    * POST /api/itens: Adiciona um novo item ao Array Map.
+    * PUT /api/itens/{id}: Atualiza o item com o índice especificado (id).
+    * DELETE /api/itens/{id}: Remove o item com o índice especificado (id).
+4. Teste a API com ferramentas como Postman ou curl.
+ */
+@RestController
+public class ItemController {
+    //retorna todos os itens
+    //@GetMapping("/item")
+
+    //retorna um item específico
+    //@GetMapping("/item/{id}")
+
+    //adiciona um item
+    //@PostMapping("/item")
+
+    //atualiza um item
+    //@PutMapping("/item/{id}")
+
+    //deleta um item
+    //@DeleteMapping("/item/{id}")
+}
